3b6d1f0f4deec92fda695aa3000fe584496a9436
AilysGeister <samantha.guivret@gmail.com>
2026-06-01 23:06:51 +0200

    menu admin

---
+++ src/app/admin-pages/menu/menu.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  isMenuOpen = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
